bdf90a8b622f2cb22a5fc306de5fec7af27717ea
feng <631676736@qq.com>
2018-05-23 18:15:27 +0800

    轮播图调整

--- 20180511-RotationMap/js/demo.js
+++ 20180511-RotationMap/js/demo.js
@@ -13,29 +13,62 @@
     }, {
         url: './img/1.jpg'
     }],
+    curIndex: 1,
+    // len: this.imgInfoArr.length,
 
 
 
     init: function () {
         this.createElements(this.imgInfoArr);
+        this.bindEvent();
     },
 
     createElements: function (data) {
-        var $img = $('<ul>', {
-                'class': 'show-img'
-            }),
-            $order = $('<ul>'),
-            len = data.length;
+        var $img = $('<div class="show-img"><ul></ul></div>'),
+            $order = $('<div class="order"><ul></ul></div>');
+        var that=this;
+        that.len = data.length;
+        // 设置包裹ul的div的尺寸
+        $img.find('.show-img').css({
+            'width': 520 * that.len + 'px',
+            height: '280px'
+        });
 
         data.forEach(function (item, index) {
-            $img.append("<li><img src='" + item.url + "'/></li>");
-            if (index < len-1) {
-                $order.append("<li></li>");
+            $img.find('ul').append("<li><img src='" + item.url + "'/></li>");
+            if (index < that.len - 1) {
+                $order.find('ul').append("<li></li>");
             }
 
         });
-        $('.wrapper').prepend($img);
-        $(".wrapper .order").append($order);
+
+        $(".wrapper").append($img, $order).find('.order li:first-child').addClass('active');
+        if (that.len > 1) {
+            $('.wrapper').append('<div class="page">\
+                                    <span class="left"></span>\
+                                    <span class="right"></span>\
+                                </div>');
+        }
+    },
+    bindEvent: function () {
+        var that = this;
+        $('.wrapper .page').on('click', 'span', function (e) {
+            if ($(this).attr('class') == 'left') {
+                that.curIndex = that.curIndex > 0 ? --that.curIndex : that.len - 1;
+            } else {
+                that.curIndex = that.curIndex < that.len - 1 ? ++that.curIndex : 0;
+            }
+        });
+
+        $('.wrapper .order').on('click', 'li', function (e) {
+            that.curIndex = $(this).index();
+        });
+
+        that.showImg(that.curIndex);
+    },
+    showImg: function (index) {
+        $('.wrapper .show-img').css('left', -index * 520 + 'px');
+        $('.wrapper .order li').find('.active').removeClass('active').eq(index).addClass('active');
     }
 
 
