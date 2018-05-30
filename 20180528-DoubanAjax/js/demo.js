b28c3e403d7fb3c2910537ee123ce3dd83f1ff41
hp g32 <yanglifengkl@qq.com>
2018-05-30 21:36:35 +0800

    豆瓣Ajax功能完成

--- 20180528-DoubanAjax/js/demo.js
+++ 20180528-DoubanAjax/js/demo.js
@@ -1,28 +1,37 @@
 (function () {
-    function ajaxData(type, url, data, datatype, successFn) {
+    function ajaxData(type, url, data, dataType, successFn) {
 
         $.ajax({
             type: type,
             url: url,
             data: data,
-            datatype: datatype,
+            dataType: dataType,
             success: successFn
         })
     }
-    $('.search input').on('input', function (e) {
-        console.log(this.value);
-        var value = this.value,
-        timer,
-            searchStr = 'count=10&q=' + value;
 
-        function handler(data) {
-            var data = JSON.parse(data);
-            console.log(data);
+    function handler(data) {
+        // console.log(data); // 返回的就是对象
+        // var repObj = JSON.parse(data);
+        var resArr = data.musics;
+        var $ul = $('<ul>');
+        if (resArr.length > 0) {
+            resArr.forEach(function (elem) {
+                $ul.append('<li><a href="' + elem.alt + '"><img src="' + elem.image + '"><span>' + elem.author[0].name + '</span></a></li>')
+            });
+            $('.suggest').empty().append($ul).css('display', 'block').slideDown('slow');
+
         }
-        clearTimeout(timer);
-        timer=setTimeout(function () {
-            ajaxData('GET', 'https://api.douban.com/v2/music/search', searchStr, 'jsonp', handler);
+    }
 
-        }, 30)
+    var timer;
+    $('.search input').on('input', function (e) {
+        // console.log(this.value);
+        clearTimeout(timer);
+        var value = this.value,
+            searchStr = 'q=' + value + '&count=5';
+        timer = setTimeout(function () {
+            ajaxData('GET', 'https://api.douban.com/v2/music/search', searchStr, 'jsonp', handler);
+        }, 800)
     });
 }())
