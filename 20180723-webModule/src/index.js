ddb91ae66a5d9d3c7048deee0100d3a3d6b664c2
feng <yanglifengwy@126.com>
2018-07-26 17:27:41 +0800

    增加webpack的学习、美团移动端项目工程化

---
+++ 20180723-webModule/src/index.js
@@ -0,0 +1,9 @@
+function a(){
+    console.log('a');
+}
+
+a();
+
+var demo = require('./demo.js');
+console.log(demo.add(123,321));
+
---
+++ 20180723-webModule/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports={
+    entry:'./src/demo.js', //指定入口
+    output:{
+        path:
+    },
+    loader:
+    plugin:
+}
